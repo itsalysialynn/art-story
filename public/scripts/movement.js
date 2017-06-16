9f34d9a2331842f17a37efee703edc07297c12c7
Alysia Lynn <itsalysialynn@gmail.com>
2017-06-16 14:13:42 -0700

    added sass files

---
+++ public/scripts/movement.js
@@ -0,0 +1,12 @@
+$(function() {
+  $(".art-movement-image").hide();
+  $(".art-movement-description").hide();
+
+  //when a movement is pressed, show the image and description
+  $(".continue-button").click(function() {
+    $(".art-movement-image").show();
+    $(".art-movement-description").show();
+
+
+
+});
