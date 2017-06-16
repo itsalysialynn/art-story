2a2ec5572bb31e48bf3dca3af263336067729623
ericalapadatjanzen@gmail.com <ericalapadatjanzen@gmail.com>
2017-06-16 16:10:28 -0700

    refactored search bar to get from search, returns jason object with results

--- server.js
+++ server.js
@@ -18,35 +18,37 @@ const traverson = require('traverson'),
 traverson.registerMediaType(JsonHalAdapter.mediaType, JsonHalAdapter);
 const api = traverson.from("https://api.artsy.net/api").jsonHal();
 
-api
-  .newRequest()
-  .follow("artist")
-  .withRequestOptions({
-    headers: {
-      "X-Xapp-Token": xappToken,
-      Accept: "application/vnd.artsy-v2+json"
-    }
-  })
-  .withTemplateParameters({ id: "andy-warhol" })
-  .getResource(function(error, artist) {
-    console.log(
-      artist.name +
-        "was born in " +
-        artist.birthday +
-        " in " +
-        artist.hometown
-    );
-  });
+// api
+//   .newRequest()
+//   .follow("artist")
+//   .withRequestOptions({
+//     headers: {
+//       "X-Xapp-Token": xappToken,
+//       Accept: "application/vnd.artsy-v2+json"
+//     }
+//   })
+//   .withTemplateParameters({ id: "andy-warhol" })
+//   .getResource(function(error, artist) {
+//     console.log(
+//       artist.name +
+//         "was born in " +
+//         artist.birthday +
+//         " in " +
+//         artist.hometown
+//     );
+//   });
 
 app.get('/search', (req, res) =>{
 
-  const artist = (req.query.search).split(" ").join("-").toLowerCase();
-  // artist.split(" ").join("-");
-  console.log(artist);
+  const input = (req.query.search)
+  // .split(" ").join("-").toLowerCase();
+  console.log(input);
 
   api
     .newRequest()
-    .follow("artist")
+    .follow("search")
+    // .follow("artworks")
+    // .follow("genes")
     .withRequestOptions({
       headers: {
         "X-Xapp-Token": xappToken,
@@ -54,17 +56,68 @@ app.get('/search', (req, res) =>{
       }
     })
 
-    .withTemplateParameters({id: artist})
+    .withTemplateParameters({q: input})
 
 
-    .getResource(function(error, artist) {
-    console.log(artist)
-     res.render('results', {artist});
+    .getResource(function(error, results) {
+    console.log(results)
+     res.render('results', {results});
 
 
     });
-  // res.send(req.query.search);
 })
+
+// app.get('/search', (req, res) =>{
+
+//   const artwork = (req.query.search).split(" ").join("-").toLowerCase();
+//   console.log(artwork);
+
+//   api
+//     .newRequest()
+//     .follow("artwork")
+//     .withRequestOptions({
+//       headers: {
+//         "X-Xapp-Token": xappToken,
+//         Accept: "application/vnd.artsy-v2+json"
+//       }
+//     })
+
+//     .withTemplateParameters({id: artwork})
+
+
+//     .getResource(function(error, artwork) {
+//     console.log(artwork)
+//      res.render('results', {artwork});
+
+//     });
+// })
+
+// app.get('/search', (req, res) =>{
+
+//   const gene = (req.query.search).split(" ").join("-").toLowerCase();
+//   console.log(gene);
+
+//   api
+//     .newRequest()
+//     .follow("gene")
+//     .withRequestOptions({
+//       headers: {
+//         "X-Xapp-Token": xappToken,
+//         Accept: "application/vnd.artsy-v2+json"
+//       }
+//     })
+
+//     .withTemplateParameters({id: gene})
+
+
+//     .getResource(function(error, gene) {
+//     console.log(gene)
+//      res.render('results', {gene});
+
+
+//     });
+// })
+
 
 app.set("view engine", "ejs");
 
