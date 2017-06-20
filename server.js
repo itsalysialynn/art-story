0c27bcedc1469301ebda8018fa35cbc315681aad
Alexandra Sia <alexandrasia@gmail.com>
2017-06-20 11:08:56 -0700

    Comment out function that does not work yet

--- server.js
+++ server.js
@@ -78,53 +78,53 @@ function getInfo(results) {
   });
 }
 
-function hasArtworkDate(x){
-  if (!x.date){
-    return false;
-  } else if (x.date){
-    return true;
-  }
-}
+// function hasArtworkDate(x){
+//   if (!x.date){
+//     return false;
+//   } else if (x.date){
+//     return true;
+//   }
+// }
 
-function mapArtistsArtworks(artistsArtworks){
-  if (!Array.isArray(artistsArtworks)){
-    artistsArtworks = [artistsArtworks];
-  }
- return artistsArtworks.filter(hasArtworkDate).map(function(x){
-  return {id: x.id, content: x.title, start: x.date.match(/\d+/)[0]}
- })
-}
+// function mapArtistsArtworks(artistsArtworks){
+//   if (!Array.isArray(artistsArtworks)){
+//     artistsArtworks = [artistsArtworks];
+//   }
+//  return artistsArtworks.filter(hasArtworkDate).map(function(x){
+//   return {id: x.id, content: x.title, start: x.date.match(/\d+/)[0]}
+//  })
+// }
 
-// accessing the artist's artworks using the artist id
-function getArtistsArtworks(results) {
+// // accessing the artist's artworks using the artist id
+// function getArtistsArtworks(results) {
 
-  return new Promise ((resolve, reject) => {
-  const artist_id = results._embedded.results[0]._links.self.href.substring(api_path.length + 9);
-  // console.log(artist_id);
-  //handles artist specific searches
-    api
-      .newRequest()
-      .follow('artworks')
-      .withTemplateParameters({artist_id: artist_id})
-      .withRequestOptions({
-        headers: {
-          "X-Xapp-Token": xappToken,
-         Accept: "application/vnd.artsy-v2+json"
-        }
-      })
+//   return new Promise ((resolve, reject) => {
+//   const artist_id = results._embedded.results[0]._links.self.href.substring(api_path.length + 9);
+//   // console.log(artist_id);
+//   //handles artist specific searches
+//     api
+//       .newRequest()
+//       .follow('artworks')
+//       .withTemplateParameters({artist_id: artist_id})
+//       .withRequestOptions({
+//         headers: {
+//           "X-Xapp-Token": xappToken,
+//          Accept: "application/vnd.artsy-v2+json"
+//         }
+//       })
 
-    .getResource((error, artists_artworks)=> {
-      const artistsArtworks = artists_artworks._embedded.artworks;
-      // console.log(artists_artworks._embedded.artworks);
+//     .getResource((error, artists_artworks)=> {
+//       const artistsArtworks = artists_artworks._embedded.artworks;
+//       // console.log(artists_artworks._embedded.artworks);
 
-      if(error) {
-        reject(error);
-      } else {
-        resolve(artistsArtworks);
-      }
-    });
-  });
-}
+//       if(error) {
+//         reject(error);
+//       } else {
+//         resolve(artistsArtworks);
+//       }
+//     });
+//   });
+// }
 
 
 // accessing similar artists with the artist id
