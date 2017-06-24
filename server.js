d574a20a35436f3233b4d76515930dabf53a2c7f
Alysia Lynn <itsalysialynn@gmail.com>
2017-06-24 12:00:28 -0700

    fixed styling and wiki links

--- server.js
+++ server.js
@@ -103,12 +103,11 @@ function getInfo(results) {
 
 // Accesses the artist's artworks using the artist id
 function getArtistsArtwork(results) {
-  if (results.birthday === "") {
-    reject("Error, please enter a valid artist or artwork");
-    return;
-  }
-
   return new Promise((resolve, reject) => {
+    if (results.birthday === "") {
+      reject("Error, please enter a valid artist or artwork");
+      return;
+    }
     const artist_id = results.id;
 
     // handles artist specific searches
@@ -256,6 +255,9 @@ function artistForVis(artist) {
 
 // Gets each artwork ready for Vis
 function map_artworks(artworks) {
+  if (!Array.isArray(artworks)) {
+    artworks = [artworks];
+  }
   return artworks.filter(has_date).map(function(x) {
     const imageLink = x._links.image.href;
     const largeImage = imageLink.replace("{image_version}", "large");
